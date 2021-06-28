93c614a9015d6743fdade202d0ea163d28f7de68
officialcody <sparshgoelt26@gmail.com>
2021-06-28 15:01:30 +0530

    Added ability to delete a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :load_task, only: %i[show update]
+  before_action :load_task, only: %i[show update destroy]
 
   def show
     render status: :ok, json: { task: @task }
@@ -25,6 +25,15 @@ class TasksController < ApplicationController
       render status: :ok, json: { notice: 'Successfully updated task.' }
     else
       render status: :unprocessable_entity, json: { errors: @task.errors.full_messages }
+    end
+  end
+
+  def destroy
+    if @task.destroy
+      render status: :ok, json: { notice: 'Successfully deleted task.' }
+    else
+      render status: :unprocessable_entity, json: { errors:
+      @task.errors.full_messages }
     end
   end
 
