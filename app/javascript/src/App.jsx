d512681046b1be6a3906387af01c0b83b46be119
officialcody <sparshgoelt26@gmail.com>
2021-06-29 12:07:06 +0530

    Added login feature

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { either, isEmpty, isNil } from "ramda";
 
 import { initializeLogger } from "common/logger";
 import { registerIntercepts, setAuthHeaders } from "apis/axios";
+import { getFromLocalStorage } from "helpers/storage";
 
+import Login from "components/Authentication/Login";
 import Signup from "components/Authentication/Signup";
+import PrivateRoute from "components/Common/PrivateRoute";
+
 import Dashboard from "components/Dashboard";
 import CreateTask from "components/Tasks/CreateTask";
 import ShowTask from "components/Tasks/ShowTask";
@@ -14,6 +19,8 @@ import PageLoader from "components/PageLoader";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const authToken = getFromLocalStorage("authToken");
+  const isLoggedIn = !either(isNil, isEmpty)(authToken) && authToken !== "null";
 
   useEffect(() => {
     initializeLogger();
@@ -33,11 +40,17 @@ const App = () => {
     <Router>
       <ToastContainer />
       <Switch>
-        <Route exact path="/signup" component={Signup} />
         <Route exact path="/tasks/:slug/show" component={ShowTask} />
         <Route exact path="/tasks/:slug/edit" component={EditTask} />
         <Route exact path="/tasks/create" component={CreateTask} />
-        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute
+          path="/"
+          redirectRoute="/login"
+          condition={isLoggedIn}
+          component={Dashboard}
+        />
       </Switch>
     </Router>
   );
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   validates :name, presence: true, length: { maximum: 35 }
+
   has_many :tasks, dependent: :destroy, foreign_key: :user_id
   has_secure_password
+  has_secure_token :authentication_token
 
   validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 6 }
