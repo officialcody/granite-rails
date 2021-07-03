5dbab38067118f3046b6bc90113ba42f482c709e
officialcody <sparshgoelt26@gmail.com>
2021-07-03 18:37:14 +0530

    Added ability to send email to each user with pending tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :tasks, dependent: :destroy, foreign_key: :user_id
+  has_many :user_notifications, dependent: :destroy, foreign_key: :user_id
+  has_one  :preference, dependent: :destroy, foreign_key: :user_id
 
   validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
@@ -12,10 +14,15 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :authentication_token
   before_save :to_lowercase
+  before_create :build_default_preference
 
   private
 
   def to_lowercase
     email.downcase!
+  end
+
+  def build_default_preference
+    self.build_preference(notification_delivery_hour: Constants::DEFAULT_NOTIFICATION_DELIVERY_HOUR)
   end
 end
